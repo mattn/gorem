9a02a69ab90a1967d6817706c64f1d7ad2f8dbab
Big Sky <mattn@isokaze.(none)>
2013-04-24 11:37:35 -0700

    Multi-user

--- gorem.go
+++ gorem.go
@@ -3,68 +3,139 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"os/signal"
 	"path"
 	"strings"
+	"syscall"
+	"time"
 )
+
+type configs map[string]config
 
 type config struct {
 	Entries []*struct {
 		Path    string `json:"path"`
 		Backend string `json:"backend"`
-		proxy   *httputil.ReverseProxy
+		proxy   http.Handler
 	} `json:"entries"`
-	Root    string `json:"root"`
-	Address string `json:"address"`
+	Root     string `json:"root"`
+	Address  string `json:"address"`
+	FlagFile string `json:"flagfile"`
 }
 
 var configFile = flag.String("c", "config.json", "config file")
 
-func main() {
-	flag.Parse()
+func loadConfig() (configs, error) {
 	f, err := os.Open(*configFile)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	var c config
+	var c configs
 	err = json.NewDecoder(f).Decode(&c)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range c {
+		for _, entry := range v.Entries {
+			u, err := url.Parse(entry.Backend)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if u.Scheme == "" {
+				entry.proxy = http.FileServer(http.Dir(entry.Backend))
+			} else {
+				if !strings.HasPrefix(entry.Path, "/") {
+					entry.Path = path.Join(v.Root, "."+entry.Path)
+				} else {
+					entry.Path = path.Join(v.Root, entry.Path)
+				}
+				if !strings.HasSuffix(entry.Path, "/") {
+					entry.Path += "/"
+				}
+				u.Path = ""
+				u.RawQuery = ""
+				u.Fragment = ""
+				entry.Backend = u.String()
+				entry.proxy = httputil.NewSingleHostReverseProxy(u)
+			}
+		}
+	}
+	return c, nil
+}
+
+func main() {
+	flag.Parse()
+
+	c, err := loadConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, entry := range c.Entries {
-		u, err := url.Parse(entry.Backend)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if !strings.HasPrefix(entry.Path, "/") {
-			entry.Path = path.Join(c.Root, "."+entry.Path)
-		} else {
-			entry.Path = path.Join(c.Root, entry.Path)
-		}
-		if !strings.HasSuffix(entry.Path, "/") {
-			entry.Path += "/"
-		}
-		entry.proxy = httputil.NewSingleHostReverseProxy(u)
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		for _, entry := range c.Entries {
-			if strings.HasPrefix(r.URL.Path, entry.Path) {
-				r.URL.Path = r.URL.Path[len(entry.Path)-1:]
-				entry.proxy.ServeHTTP(w, r)
-				return
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGHUP)
+	go func() {
+		for _ = range sc {
+			log.Println("reloading configuration")
+			c, err = loadConfig()
+			if err != nil {
+				log.Println(err)
 			}
 		}
-		http.NotFound(w, r)
-	})
+	}()
 
-	if err = http.ListenAndServe(c.Address, nil); err != nil {
-		log.Fatal(err)
+	go func() {
+		tc := time.Tick(10 * time.Second)
+		for _ = range tc {
+			for _, v := range c {
+				if v.FlagFile == "" {
+					continue
+				}
+				if b, err := ioutil.ReadFile(v.FlagFile); err == nil {
+					k := strings.TrimSpace(string(b))
+					os.Remove(v.FlagFile)
+					if _, ok := c[k]; ok {
+						if nc, err := loadConfig(); err == nil {
+							log.Printf("[%s] updated configuration", k)
+							c[k] = nc[k]
+						}
+					}
+				}
+			}
+		}
+	}()
+
+	for k, v := range c {
+		mux := http.NewServeMux()
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			for _, entry := range v.Entries {
+				if strings.HasPrefix(r.URL.Path, entry.Path) {
+					forward := r.URL.Path[len(entry.Path)-1:]
+					log.Printf("[%s] %s %s => %s%s", k, r.Method, r.URL.Path, entry.Backend, forward)
+					r.URL.Path = forward
+					r.Header.Set("X-Script-Name", forward)
+					entry.proxy.ServeHTTP(w, r)
+					return
+				}
+			}
+			http.NotFound(w, r)
+		})
+		go func(mux *http.ServeMux, k string, v config) {
+			log.Printf("[%s] server %s started", k, v.Address)
+			if err = http.ListenAndServe(v.Address, mux); err != nil {
+				log.Fatal(err)
+			}
+		}(mux, k, v)
 	}
+
+	quit := make(chan bool)
+	<-quit
 }
